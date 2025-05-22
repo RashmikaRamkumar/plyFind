f1a649c84a855fb27d0ed4bffa55e166b1d6d8ef
jaisanth123 <jaisanth2006@gmail.com>
2025-05-22 07:22:12 +0530

    navbar change

--- plyFindWebsite/frontend/src/components/layout/Layout.jsx
+++ plyFindWebsite/frontend/src/components/layout/Layout.jsx
@@ -1,23 +1,21 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Navbar from "./Navbar"; 
+import Navbar from "./Navbar";
 
 const Layout = () => {
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
       <header>
-        
         <Navbar />
       </header>
-      <main>
-       
+      <main className="flex-grow pt-20">
         <Outlet />
       </main>
       <footer>
         {/* Add your footer content here */}
         <div className="bg-wood-dark text-white py-4 text-center">
           <p>&copy; 2025 Rakesh Glass & Plywood. All rights reserved.</p>
-          </div>
+        </div>
       </footer>
     </div>
   );
--- plyFindWebsite/frontend/src/components/layout/Navbar.jsx
+++ plyFindWebsite/frontend/src/components/layout/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-white shadow-md">
+    <nav className="bg-white shadow-md fixed top-0 left-0 right-0 z-50">
       <div className="container-custom py-4">
         <div className="flex justify-between items-center">
           {/* Logo */}
